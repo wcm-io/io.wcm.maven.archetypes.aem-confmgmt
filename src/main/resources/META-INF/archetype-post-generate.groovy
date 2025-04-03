065911927f0a2796105069381274d066f2789274
Stefan Seifert <stefanseifert@users.noreply.github.com>
2025-04-03 14:22:04 +0200

    enable renovate check for groovy script

--- src/main/resources/META-INF/archetype-post-generate.groovy
+++ src/main/resources/META-INF/archetype-post-generate.groovy
@@ -1,4 +1,5 @@
 @Grab(group="org.apache.groovy", module="groovy-xml", version="4.0.23")
+// renovate: depName=io.wcm.devops.conga.plugins:conga-aem-crypto-cli
 @Grab(group='io.wcm.devops.conga.plugins', module='conga-aem-crypto-cli', version='2.20.0')
 import groovy.io.FileType
 import groovy.xml.XmlSlurper
